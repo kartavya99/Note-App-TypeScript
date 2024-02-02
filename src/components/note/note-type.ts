3e7677d32c9fe475fd66a2e60a476eab38cea224
kartavya <kartavyas@gmail.com>
2024-02-02 14:57:44 +1100

    add-note compopent added

--- src/components/note/note-type.ts
+++ src/components/note/note-type.ts
@@ -1,5 +1,12 @@
+export type Priority = "high" | "medium" | "low";
+
 export type NoteProps = {
-  id?: number;
   text: string;
-  priority?: "high" | "medium" | "low";
+  priority?: Priority;
+};
+
+export type NoteType = {
+  id: string;
+  text: string;
+  priority: Priority;
 };
